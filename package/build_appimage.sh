f1d87276b1980c859a223f29c04455c8db1bf7ce
qurious-pixel <62252937+qurious-pixel@users.noreply.github.com>
2023-04-25 11:59:00 -0700

    Update build_appimage.sh

--- package/build_appimage.sh
+++ package/build_appimage.sh
@@ -33,8 +33,8 @@ cp ../bin/samrewritten AppDir/usr/bin
 cp ../bin/libsteam_api.so AppDir/usr/lib
 cp ../assets/icon_256.png AppDir/usr/assets/
 
-LD_LIBRARY_PATH=AppDir/usr/lib $LINUXDEPLOY-plugin-appimage --appdir AppDir
+LD_LIBRARY_PATH=AppDir/usr/lib $LINUXDEPLOY --appdir AppDir
 rm AppDir/usr/lib/{libglib-2.0.so.0,libgmodule-2.0.so.0}
 
-$LINUXDEPLOY --appdir AppDir
+$LINUXDEPLOY-plugin-appimage --appdir AppDir
 rm -rf squashfs-root
